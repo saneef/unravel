33c4e6c395be97ced9facd87bdeab8a6299cb37f
saneef <hello@saneef.com>
2022-05-04 23:29:10 +0530

    Upgrade NPM packages

--- eleventy/url.js
+++ eleventy/url.js
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const resolveUrl = async (url) => {
   try {
