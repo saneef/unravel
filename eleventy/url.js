0a64d8b1ba8005d26d5d6db95f8072b2566da6ed
saneef <hello@saneef.com>
2022-05-04 23:47:37 +0530

    Revert node-fetch to v2.2.2
    Dynamic import of node-fetch is broken on Netlify functions

--- eleventy/url.js
+++ eleventy/url.js
@@ -1,5 +1,4 @@
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const fetch = require("node-fetch");
 
 const resolveUrl = async (url) => {
   try {
