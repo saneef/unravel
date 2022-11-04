00a78dc47f9ca014f57d075a3d8fd9f4cd67905d
Saneef Ansari <hello@saneef.com>
2022-11-05 00:21:51 +0530

    Adds meta description tag

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: "Unravel URL",
+  description: "Gives you orignal URL from shortened or tracking URLs.",
   production: process.env.NODE_ENV === "production",
   now: new Date().toISOString(),
 };
