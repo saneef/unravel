023e4b7894c5a2dbeb41eaf29cdf70e7e7446dc2
Saneef Ansari <hello@saneef.com>
2022-11-06 22:52:53 +0530

    fix: footer layout on smaller viewports

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: "Unravel URL",
   description: "Gives you orignal URL from shortened or tracking URLs.",
-  production: process.env.NODE_ENV === "production",
+  development: process.env.NODE_ENV === "development",
   now: new Date().toISOString(),
-  serverless: process.env.ELEVENTY_SERVERLESS,
 };
