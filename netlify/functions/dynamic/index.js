f7896f674c6a150b642c82e4409aba7ac0c9c657
Saneef Ansari <hello@saneef.com>
2023-09-17 10:33:39 +0530

    Newly generated Eleventy-Netlify serverless function

--- netlify/functions/dynamic/index.js
+++ netlify/functions/dynamic/index.js
@@ -7,7 +7,7 @@ require("./eleventy-bundler-modules.js");
 async function handler(event) {
   let elev = new EleventyServerless("dynamic", {
     path: new URL(event.rawUrl).pathname,
-    query: event.queryStringParameters,
+    query: event.multiValueQueryStringParameters || event.queryStringParameters,
     functionsDir: "./netlify/functions/",
   });
 
